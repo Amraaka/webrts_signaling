779590910c2179d8f3b09f2bc689dad4643acf92
emberaidev-projects <emberai.dev@gmail.com>
2026-01-29 13:28:56 +0800

    a

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
+const http = require("http");
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 3000 });
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>WebRTC Signaling Server</title>
+    </head>
+    <body>
+      <h1>WebRTC Signaling Server</h1>
+      <p>WebSocket server is running on ws://localhost:3000</p>
+      <p>Connect using a WebSocket client to use the signaling server.</p>
+    </body>
+    </html>
+  `);
+});
+
+const wss = new WebSocket.Server({ server });
 let peers = [];
 
 wss.on("connection", ws => {
@@ -19,4 +37,7 @@ wss.on("connection", ws => {
   });
 });
 
-console.log("Signaling server running on ws://localhost:3000");
+server.listen(3000, () => {
+  console.log("Signaling server running on http://localhost:3000");
+  console.log("WebSocket endpoint: ws://localhost:3000");
+});
